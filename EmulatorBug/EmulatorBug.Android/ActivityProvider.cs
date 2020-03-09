203ca6dadcb4f5d3e0e645ce12a72cc66048d10e
John Clay <john.s.clay5.ctr@mail.mil>
2020-03-09 14:01:27 -0500

    Add project files.

---
+++ EmulatorBug/EmulatorBug.Android/ActivityProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EmulatorBug.Droid
+{
+    /// <summary>
+    /// Taken from https://byteloom.marek-mierzwa.com/mobile/2018/05/26/detecting-on-screen-keyboard-toggle.html
+    /// </summary>
+    public class ActivityProvider
+    {
+        public static Activity CurrentActivity { get; set; }
+        public static View RootContentView
+            => CurrentActivity.FindViewById(Android.Resource.Id.Content);
+    }
+}
---
+++ EmulatorBug/EmulatorBug.Android/KeyboardNotificationProvider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+using Android.Views.InputMethods;
+using Xamarin.Forms;
+
+namespace EmulatorBug.Droid
+{
+    /// <summary>
+    /// Adapted from https://byteloom.marek-mierzwa.com/mobile/2018/05/26/detecting-on-screen-keyboard-toggle.html
+    /// </summary>
+    public class KeyboardNotificationProvider
+    {
+        private GlobalLayoutListener _globalLayoutListener;
+
+        public void StartNotifying()
+        {
+            if (_globalLayoutListener == null)
+            {
+                _globalLayoutListener = new GlobalLayoutListener();
+                ActivityProvider.RootContentView.ViewTreeObserver.AddOnGlobalLayoutListener(_globalLayoutListener);
+            }
+        }
+
+        public void StopNotifying()
+        {
+            ActivityProvider.RootContentView.ViewTreeObserver.RemoveOnGlobalLayoutListener(_globalLayoutListener);
+        }
+    }
+
+    internal class GlobalLayoutListener : Java.Lang.Object, ViewTreeObserver.IOnGlobalLayoutListener
+    {
+        /// <summary>
+        /// https://byteloom.marek-mierzwa.com/mobile/2018/05/26/detecting-on-screen-keyboard-toggle.html
+        /// https://www.codeproject.com/Articles/1172935/Detecting-Software-Keyboard-Events-in-Xamarin-Andr
+        /// </summary>
+        private static InputMethodManager _inputManager;
+
+        private bool IsShowing;
+
+        public GlobalLayoutListener()
+        {
+            _inputManager = ObtainInputManager();
+        }
+
+        public void OnGlobalLayout()
+        {
+            if (_inputManager.Handle == IntPtr.Zero)
+                _inputManager = ObtainInputManager();
+
+            if (_inputManager.IsAcceptingText)
+                SendKeyboardShownNotification();
+            else
+                SendKeyboardHiddenNotification();
+        }
+
+        public void SendKeyboardHiddenNotification()
+        {
+            if (IsShowing)
+            {
+                IsShowing = false;
+                MessagingCenter.Send<object>(this, "KeyboardHidden");
+            }
+        }
+        public void SendKeyboardShownNotification()
+        {
+            if (!IsShowing)
+            {
+                IsShowing = true;
+                MessagingCenter.Send<object>(this, "KeyboardShown");
+            }
+        }
+
+        private static InputMethodManager ObtainInputManager()
+        {
+            return (InputMethodManager)ActivityProvider.CurrentActivity.GetSystemService(Context.InputMethodService);
+        }
+    }
+}
